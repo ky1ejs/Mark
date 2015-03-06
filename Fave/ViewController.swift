89cecff38ae6b62ecbad90c51b0ef12c3d9d2b40
Kyle McAlpine <k@kylejm.io>
2015-03-06 13:08:14 +0000

    Initial Commit

---
+++ Fave/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fave
+//
+//  Created by Kyle McAlpine on 06/03/2015.
+//  Copyright (c) 2015 kylejm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
