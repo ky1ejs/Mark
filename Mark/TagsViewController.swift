a398efff25aba55b0527b802a15b1ea5eff798cc
Kyle McAlpine <k@kylejm.io>
2015-04-29 04:10:21 +0100

    WIP: Started creating a collection view for the tags

---
+++ Mark/TagsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TagsViewController.swift
+//  Mark
+//
+//  Created by Kyle McAlpine on 29/04/2015.
+//  Copyright (c) 2015 kylejm. All rights reserved.
+//
+
+import Cocoa
+
+class TagsViewController: NSViewController {
+    @IBOutlet weak var collectionView : NSCollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
